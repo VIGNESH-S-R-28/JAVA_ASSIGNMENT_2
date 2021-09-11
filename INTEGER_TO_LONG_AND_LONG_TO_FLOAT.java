3d00aae3fd89fc3206e69ed152e81737724a88a1
VIGNESH RAJENDRAN <89457100+VIGNESH-S-R-28@users.noreply.github.com>
2021-09-11 21:17:14 +0530

    INTEGER_TO_LONG_AND_LONG_TO_FLOAT

---
+++ INTEGER_TO_LONG_AND_LONG_TO_FLOAT.java
@@ -0,0 +1,14 @@
+
+public class INTEGER_TO_LONG_AND_LONG_TO_FLOAT {
+
+	public static void main(String[] args) {
+		
+		int i=500;  
+		long l=i;
+		float F = l;
+		System.out.println("Integer_value :" +i);
+		System.out.println("Long_value :" +l);
+		System.out.println("Float_value :" +F);
+	}
+
+}
